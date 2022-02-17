cf94490a834f53afbf4b4cd0ca9fd5d10ea56c6e
unknown <wjdtjrdn125@naver.com>
2022-02-17 23:53:52 +0900

    20220217

---
+++ seokwoo/programmerLv1/Main.java
@@ -0,0 +1,11 @@
+package seokwoo.programmerLv1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Kakao2 kakao2 = new Kakao2();
+		System.out.println(kakao2.solution("abcdefghijklmn.p"));
+
+	}
+
+}
