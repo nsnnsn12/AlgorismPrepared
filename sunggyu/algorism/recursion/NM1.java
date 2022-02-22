65fd351577362d77bbc06b8bf76bcd3be2d24603
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-02-23 08:33:30 +0900

    재귀1번 문제 리팩토링

--- sunggyu/algorism/recursion/NM1.java
+++ sunggyu/algorism/recursion/NM1.java
@@ -4,20 +4,25 @@ import java.util.*;
 //https://www.acmicpc.net/problem/15649
 //N과 M(1)
 public class NM1 {
+    public static StringBuilder sb;
+    public static int n;
+    public static int m;
+    public static boolean[] selectFlag;
+    public static char[] result;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        sb = new StringBuilder();
         String str = sc.nextLine();
         String[] split = str.split(" ");
-        int n = Integer.parseInt(split[0]);
-        int m = Integer.parseInt(split[1]);
-        boolean[] selectFlag = new boolean[n];
-        int[] list = new int[m];
-        List<Integer> list2 = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            list2.add(i+1);
+        n = Integer.parseInt(split[0]);
+        m = Integer.parseInt(split[1]);
+        selectFlag = new boolean[n];
+        result = new char[2*m];
+        for(int i = 1; i < 2*m; i+=2){
+            result[i] = ' ';
         }
-        String result = "";
-        permutation(n, m, 0, selectFlag, list, list2, result);
+        permutation2(0);
+        System.out.print(sb);
     }
 
     public static void permutation(int n, int m, int index, boolean[] selectFlag, int[] list, List<Integer> list2, String result){
@@ -36,6 +41,22 @@ public class NM1 {
             list2.add(value);
             Collections.sort(list2);
             result = result.substring(0, result.length()-2);
+        }
+    }
+
+    public static void permutation2(int depth){
+        if(depth == m){
+            sb.append(result).append('\n');
+            return;
+        }
+        for(int i = 0; i < n; i++){
+            if(!selectFlag[i]){
+                selectFlag[i] = true;
+                int value = i+1;
+                result[depth*2] = (char)(value+'0');
+                permutation2(depth+1);
+                selectFlag[i] = false;
+            }
         }
     }
 }
