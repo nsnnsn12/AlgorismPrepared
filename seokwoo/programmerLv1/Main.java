4438edc8cb52c66f7a9cccd7bb3ded0538437e1f
unknown <wjdtjrdn125@naver.com>
2022-02-27 16:33:14 +0900

    0227

--- seokwoo/programmerLv1/Main.java
+++ seokwoo/programmerLv1/Main.java
@@ -1,10 +1,13 @@
 package seokwoo.programmerLv1;
 
 public class Main {
-
 	public static void main(String[] args) {
-		Kakao2 kakao2 = new Kakao2();
-		System.out.println(kakao2.solution("abcdefghijklmn.p"));
+		Kakao3 kakao3 = new Kakao3();
+		String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+		String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi","muzi frodo"};
+		int k = 2;
+				
+		System.out.println(kakao3.solution(id_list,report,k));
 
 	}
 
