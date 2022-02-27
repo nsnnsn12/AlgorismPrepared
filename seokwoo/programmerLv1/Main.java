e4d9e8bc31fc64ca9821c799e66971ce6b853894
unknown <wjdtjrdn125@naver.com>
2022-02-27 17:19:19 +0900

    0227

--- seokwoo/programmerLv1/Main.java
+++ seokwoo/programmerLv1/Main.java
@@ -2,12 +2,10 @@ package seokwoo.programmerLv1;
 
 public class Main {
 	public static void main(String[] args) {
-		Kakao3 kakao3 = new Kakao3();
-		String[] id_list = {"muzi", "frodo", "apeach", "neo"};
-		String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi","muzi frodo"};
-		int k = 2;
-				
-		System.out.println(kakao3.solution(id_list,report,k));
+		Kakao4 kakao4 = new Kakao4();
+		int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+		int[] moves = 	{1,5,3,5,1,2,1,4};
+		System.out.println(kakao4.solution(board,moves));
 
 	}
 
