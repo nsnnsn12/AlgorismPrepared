cd2086795b68b8980937e87ef05aed9b0a42676c
unknown <wjdtjrdn125@naver.com>
2022-03-06 16:09:46 +0900

    0306

--- seokwoo/programmerLv1/Main.java
+++ seokwoo/programmerLv1/Main.java
@@ -2,10 +2,10 @@ package seokwoo.programmerLv1;
 
 public class Main {
 	public static void main(String[] args) {
-		Kakao4 kakao4 = new Kakao4();
-		int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
-		int[] moves = 	{1,5,3,5,1,2,1,4};
-		System.out.println(kakao4.solution(board,moves));
+		Kakao5 kakao5 = new Kakao5();
+		int[] numbers = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+		String hand = 	"left";
+		System.out.println(kakao5.solution(numbers,hand));
 
 	}
 
