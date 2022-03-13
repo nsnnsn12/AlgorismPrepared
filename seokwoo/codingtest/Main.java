9a2c0da12305186138a665bf5894bf16133005a7
unknown <wjdtjrdn125@naver.com>
2022-03-13 18:44:58 +0900

    3월 2째주

---
+++ seokwoo/codingtest/Main.java
@@ -0,0 +1,15 @@
+package seokwoo.codingtest;
+
+import seokwoo.codingtest.sk220312.SK1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SK1 sk1 = new SK1();
+		int money = 4578;
+		int[] costs = {1,4,99,35,50,1000};
+		sk1.solution(money, costs);
+
+	}
+
+}
