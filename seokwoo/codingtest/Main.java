c68cd0a38ea3adce60508b6d9ec3cd320b7b77fb
unknown <wjdtjrdn125@naver.com>
2022-04-03 17:31:04 +0900

    4월 첫째주

--- seokwoo/codingtest/Main.java
+++ seokwoo/codingtest/Main.java
@@ -1,15 +1,16 @@
 package seokwoo.codingtest;
 
+import seokwoo.codingtest.backendDeveloper220402.BackEndDeveloper1;
+import seokwoo.codingtest.backendDeveloper220402.BackEndDeveloper2;
 import seokwoo.codingtest.sk220312.SK1;
 import seokwoo.codingtest.sk220312.SK2;
 
 public class Main {
 
 	public static void main(String[] args) {
-		SK2 sk2 = new SK2();
-		int n = 9;
-		boolean clockwise = false;
-		sk2.solution(n, clockwise);
+		BackEndDeveloper1 backEndDeveloper1 = new BackEndDeveloper1();
+		int[][] dist = {{0,5,2,4,1,3},{0,0,3,9,6},{0,0,0,6,3},{0,0,0,0,3},{0,0,0,0,0}};
+		backEndDeveloper1.solution(dist);
 
 	}
 
