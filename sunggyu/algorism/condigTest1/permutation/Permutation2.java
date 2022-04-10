a6dd3d0a6cd1ea1dd0257ef29539602e807c8920
nohsunggyu <gkdlshtjdrb@naver.com>
2022-04-11 06:59:27 +0900

    코딩테스트 기초 순열 2번 문제 해결

--- sunggyu/algorism/condigTest1/permutation/Permutation2.java
+++ sunggyu/algorism/condigTest1/permutation/Permutation2.java
@@ -2,20 +2,12 @@ package sunggyu.algorism.condigTest1.permutation;
 import java.io.*;
 import java.util.*;
 import java.util.stream.Stream;
-//https://www.acmicpc.net/problem/10972
-//다음 순열
+//https://www.acmicpc.net/problem/10973
+//이전 순열
 /*
-    순열을 오름차순으로 뽑는다고 했을 때
-    맨 첫번째는 오름차순이고
-    마지막은 내림차순이다.
-
-    순열을 오름차순으로 뽑는다고 했을 때 
-    항상 선택하지 않은 것 중에서 제일 작은 것부터 골라 나간다.
-
-    마지막으로 정렬되어 있는 부분을 기준으로 값을 다시 뽑으면 다음 순열을 찾을 수 있다.
 
 */
-public class Permutation1{
+public class Permutation2{
     static int n;
     static int[] list;
     static boolean[] visit;
@@ -31,7 +23,7 @@ public class Permutation1{
         }
         int index = 0;
         for(int i = n-1; i > 0; i--){
-            if(list[i]> list[i-1]){
+            if(list[i] < list[i-1]){
                 index = i;
                 break;
             }
@@ -39,13 +31,15 @@ public class Permutation1{
         String result = "-1";
 
         if(index != 0){
+            index--;
+
             visit = new boolean[n];
             Arrays.fill(visit, true);
-            for(int i = index - 1; i < n; i++){
+            for(int i = index; i < n; i++){
                 visit[list[i]] = false;
             }
 
-            dfs(index - 1, list[index-1]+1);
+            dfs(index, list[index]-1);
 
             StringBuilder sb = new StringBuilder();
             Arrays.stream(list).forEach(n ->{
@@ -64,11 +58,11 @@ public class Permutation1{
         if(depth == n){
             return;
         }
-        for(int i = startIndex; i < n; i++){
+        for(int i = startIndex; i >= 0; i--){
             if(!visit[i]){
                 visit[i] = true;
                 list[depth] = i;
-                dfs(depth + 1, 0);
+                dfs(depth + 1, n-1);
             }
         }
         
