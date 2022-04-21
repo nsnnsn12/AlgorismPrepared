b0b788370f5420edd4014ad9bdaeef7be08478c2
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-04-22 08:29:59 +0900

    코딩테스트 기초 구현 2번 문제 시간 리팩토링 중

--- sunggyu/algorism/condigTest1/implement/Implement2.java
+++ sunggyu/algorism/condigTest1/implement/Implement2.java
@@ -9,6 +9,7 @@ public class Implement2{
     static int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
     static int n;
     static int m;
+    static int r;
     static int[][] map;
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -16,7 +17,7 @@ public class Implement2{
         String[] nmr = bf.readLine().split(" ");
         n = Integer.parseInt(nmr[0]);
         m = Integer.parseInt(nmr[1]);
-        int r = Integer.parseInt(nmr[2]);
+        r = Integer.parseInt(nmr[2]);
 
         map = new int[n][m];
         for(int i = 0; i < n; i++){
@@ -25,9 +26,9 @@ public class Implement2{
                 map[i][j] = Integer.parseInt(split[j]);
             }
         }
-        for(int i = 0; i < r; i++){
-            rotate(map);
-        }
+
+        rotate(map);
+        
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 System.out.print(map[i][j] + " ");
@@ -37,15 +38,41 @@ public class Implement2{
         bw.flush();
         bw.close();
     }
+    public static List<Integer> getList(int[] firstNM, int[] lastNM){
+        int nowX = firstNM[0];
+        int nowY = firstNM[1];
+        int startX = firstNM[0];
+        int startY = firstNM[1];
+        int endX = lastNM[0];
+        int endY = lastNM[1];
+        List<Integer> list = new ArrayList<>();
+        list.add(map[nowX][nowY]);
+        for(int[] direct : directions){
+            while(true){
+                int nx = nowX + direct[0];
+                int ny = nowY + direct[1];
+                if(nx < startX || nx >= endX || ny < startY || ny >= endY){
+                    break;
+                }
+                nowX = nx;
+                nowY = ny;
+                list.add(map[nowX][nowY]);
+            }
+        }
 
+        return list;
+
+    }
     public static void rotate(int[][] map){
         int[] firstNM = {0,0};
         int[] lastNM = {n, m};
-
+        
         while(true){
             if(firstNM[0] >= lastNM[0] || firstNM[1] >= lastNM[1]) break;
+            List<Integer> list = getList(firstNM, lastNM);
 
-            swap(map, firstNM, lastNM);
+            
+
             firstNM[0]++;
             firstNM[1]++;
             lastNM[0]--;
@@ -53,7 +80,7 @@ public class Implement2{
         }
     }
 
-    public static void swap(int[][] map, int[] firstNM, int[] lastNM){
+    public static void swap(int[] firstNM, int[] lastNM){
         int nowX = firstNM[0];
         int nowY = firstNM[1];
         int startX = firstNM[0];
