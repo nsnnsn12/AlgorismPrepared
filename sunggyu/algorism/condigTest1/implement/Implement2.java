3e4651694753dead0d70fade9c4e60797576f3d2
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-04-22 08:17:37 +0900

    코딩테스트 기초 구현 2번 문제 메모리 리팩토링

--- sunggyu/algorism/condigTest1/implement/Implement2.java
+++ sunggyu/algorism/condigTest1/implement/Implement2.java
@@ -7,15 +7,18 @@ import java.util.*;
 */
 public class Implement2{
     static int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
+    static int n;
+    static int m;
+    static int[][] map;
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] nmr = bf.readLine().split(" ");
-        int n = Integer.parseInt(nmr[0]);
-        int m = Integer.parseInt(nmr[1]);
+        n = Integer.parseInt(nmr[0]);
+        m = Integer.parseInt(nmr[1]);
         int r = Integer.parseInt(nmr[2]);
 
-        int[][] map = new int[n][m];
+        map = new int[n][m];
         for(int i = 0; i < n; i++){
             String[] split = bf.readLine().split(" ");
             for(int j = 0; j < m; j++){
@@ -23,7 +26,7 @@ public class Implement2{
             }
         }
         for(int i = 0; i < r; i++){
-            map = turnLeft(map);
+            rotate(map);
         }
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
@@ -35,31 +38,29 @@ public class Implement2{
         bw.close();
     }
 
-    public static int[][] turnLeft(int[][] map){
+    public static void rotate(int[][] map){
         int[] firstNM = {0,0};
-        int[] lastNM = {map.length, map[0].length};
-        int[][] tempMap = new int[lastNM[0]][lastNM[1]];
+        int[] lastNM = {n, m};
+
         while(true){
             if(firstNM[0] >= lastNM[0] || firstNM[1] >= lastNM[1]) break;
 
-            swap(map, tempMap, firstNM, lastNM);
+            swap(map, firstNM, lastNM);
             firstNM[0]++;
             firstNM[1]++;
             lastNM[0]--;
             lastNM[1]--;
         }
-
-        return tempMap;
     }
 
-    public static void swap(int[][] map, int[][] tempMap, int[] firstNM, int[] lastNM){
+    public static void swap(int[][] map, int[] firstNM, int[] lastNM){
         int nowX = firstNM[0];
         int nowY = firstNM[1];
         int startX = firstNM[0];
         int startY = firstNM[1];
         int endX = lastNM[0];
         int endY = lastNM[1];
-
+        int temp = map[nowX][nowY];
         for(int[] direct : directions){
             while(true){
                 int nx = nowX + direct[0];
@@ -67,10 +68,11 @@ public class Implement2{
                 if(nx < startX || nx >= endX || ny < startY || ny >= endY){
                     break;
                 }
-                tempMap[nowX][nowY] = map[nx][ny];
+                map[nowX][nowY] = map[nx][ny];
                 nowX = nx;
                 nowY = ny;
             }
         }
+        map[startX+1][startY] = temp;
     }
 }
