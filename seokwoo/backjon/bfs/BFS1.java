4df768a0d5b865c124fa6805206fb11fd9e93fff
unknown <wjdtjrdn125@naver.com>
2022-04-30 22:07:49 +0900

    4월5째주

---
+++ seokwoo/backjon/bfs/BFS1.java
@@ -0,0 +1,54 @@
+package seokwoo.backjon.bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS1 {
+	static int[] matrix;
+	static int k;
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		matrix = new int[100001];
+		String[] def = in.readLine().split(" ");
+		
+		int n = Integer.parseInt(def[0]);
+		k = Integer.parseInt(def[1]);
+		
+		bfs(n);
+		System.out.println(matrix[k]);
+
+	}
+	private static void bfs(int start) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(start);
+		while(!queue.isEmpty()) {
+			int x = queue.poll();
+			
+			if(x == k) {
+				break;
+			}
+			
+			for(int i =0; i<3; i++) {
+				int nx = 0;
+				if(i == 0) {
+					nx = x-1;
+				}else if(i==1) {
+					nx = x+1;
+				}else {
+					nx = x*2;
+				}
+				if(0<=nx && nx<=100000) {
+					if(matrix[nx] == 0) {
+						queue.offer(nx);
+						matrix[nx] = matrix[x] + 1;
+					}
+				}
+			}
+		}
+		
+	}
+
+}
