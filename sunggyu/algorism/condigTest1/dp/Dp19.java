61fe37c64de3500829a09b195f9db945b17a75f7
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-05-16 08:48:37 +0900

    코딩테스트 기초 DP 19번 문제 푸는 중

--- sunggyu/algorism/condigTest1/dp/Dp19.java
+++ sunggyu/algorism/condigTest1/dp/Dp19.java
@@ -10,24 +10,28 @@ import java.util.*;
     내가 제대로 이해하지 못 하기 때문에 예외가 생긴다.
 */
 public class Dp19{
+    public static final int oneBlock = 0;
+    public static final int twoBlock = 1;
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(bf.readLine());
+        
         int[][] memo = new int[n][2];
         for(int i = 0; i < n; i++){
             int value = Integer.parseInt(bf.readLine());
             memo[i][0] = value;
             memo[i][1] = value;
         }
-
+        //memo[i][0] 인접한 index를 방문한 값
+        //memo[i][1] 한 칸 떨어진 index를 방문한 값
         for(int i = 1; i < n; i++){
             //인접한 경우
-            memo[i][0] += memo[i-1][1];
+            memo[i][twoBlock] += memo[i-1][oneBlock];
 
             //인접하지 않은 경우
             if(i - 2 >= 0){
-                memo[i][1] += Math.max(memo[i-2][0], memo[i-2][1]);
+                memo[i][oneBlock] += memo[i-2][twoBlock];
             }
             
         }
