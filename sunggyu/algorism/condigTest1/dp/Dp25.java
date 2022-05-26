5536d6091f266085e7de439f5f215669533d7fd9
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-05-27 08:50:47 +0900

    코딩테스트 기초 DP 25번 문제 푸는 중

--- sunggyu/algorism/condigTest1/dp/Dp25.java
+++ sunggyu/algorism/condigTest1/dp/Dp25.java
@@ -15,8 +15,8 @@ public class Dp25{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(bf.readLine());
         int result = 0;
-        int[] n2List = new int[n];
-        int[] n3List = new int[n];
+        long[] n2List = new long[n];
+        long[] n3List = new long[n];
         n2List[0] = 1;
         for(int i = 1; i < n; i++){
             if(i-2 < 0){
@@ -29,9 +29,13 @@ public class Dp25{
         if(n > 1){
             n3List[1] = 3;
             for(int i = 3; i < n; i+= 2){
+                n3List[i] = n3List[i-2] * 3;
+                n3List[i] += n2List[i-3] * 2;
 
             }
         }
+
+        System.out.println(n3List[n-1]);
         bw.flush();
         bw.close();
     }
