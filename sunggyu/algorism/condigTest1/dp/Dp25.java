c5a571c3a1ec0f3a99130b05457212760b83e4aa
nohsunggyu <gkdlshtjdrb@naver.com>
2022-06-01 15:10:47 +0900

    코딩테스트 기초 DP 25번 문제 해결

--- sunggyu/algorism/condigTest1/dp/Dp25.java
+++ sunggyu/algorism/condigTest1/dp/Dp25.java
@@ -5,18 +5,21 @@ import java.util.*;
 //타일 채우기
 /*
     n이 홀수인 경우는 타일로 채울 수 없다.
-    2 * n의 경우의 수를 먼저 구한다.
-    2개인 경우는 3개이다.
+    
+    길이가 2인 경우를 기준으로의 모든 경우의 수
+    n3List[i] = n3List[i-2] * 3;
+
+    그외 길이가 2의 배수인 경우
+    n3List[i] += n3List[j] * 2;
 */
 public class Dp25{
-    public static long[] n2List;
-    public static long result = 0L;
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(bf.readLine());
+        long result = 0;
         if(n % 2 == 0){
-            getResult(n);
+            result = getResult(n + 1);
         }
 
         System.out.println(result);
@@ -24,74 +27,17 @@ public class Dp25{
         bw.close();
     }
 
-    public static void getResult2(int n){
-        n2List = new long[n];
-        n2List[0] = 1L;
-        for(int i = 1; i < n; i++){
-            if(i == 1){
-                n2List[i] = 2L;
-                continue;
-            }
-            n2List[i] = n2List[i-2];
-            n2List[i] += n2List[i-1];
-        }
+    public static long getResult(int n){
         long[] n3List = new long[n];
         n3List[0] = 1;
-        n3List[1] = 3;
-        for(int i = 3; i < n; i+=2){
+        n3List[1] = 0;
+        n3List[2] = 3;
+        for(int i = 4; i < n; i+=2){
             n3List[i] = n3List[i-2] * 3;
-            long value = n2List[i];
-            value -= n2List[i-2];
-        }
-        char[] list = new char[n/2];
-        dfs(0, n/2, list);
-        result++;
-    }
-
-    public static void getResult(int n){
-        n2List = new long[n];
-        n2List[0] = 1L;
-        for(int i = 1; i < n; i++){
-            if(i == 1){
-                n2List[i] = 2L;
-                continue;
-            }
-            n2List[i] = n2List[i-2];
-            n2List[i] += n2List[i-1];
-        }
-        char[] list = new char[n/2];
-        dfs(0, n/2, list);
-        result++;
-    }
-
-    public static void dfs(int depth, int n, char[] list){
-        if(depth == n){
-            System.out.println(new String(list));
-            test(list);
-            return;
-        }
-
-        for(int i = 0; i < 2; i++){
-            list[depth] = (char)(i+'0');
-            dfs(depth+1, n, list);
-        }
-    }
-
-    public static void test(char[] list){
-        long sum = 1L;
-        char first = list[0];
-        int count = 1;
-        for(int i = 1; i < list.length; i++){
-            if(list[i] == first){
-                count++;
-            }else{
-                first = list[i];
-                sum *= n2List[count*2-1];
-                count = 1;
+            for(int j = i-4; j >= 0; j -= 2){
+                n3List[i] += n3List[j] * 2;
             }
         }
-        sum *= n2List[count*2-1];
-        result += sum-1;
-        System.out.println(sum-1);
+        return n3List[n-1];
     }
 }
