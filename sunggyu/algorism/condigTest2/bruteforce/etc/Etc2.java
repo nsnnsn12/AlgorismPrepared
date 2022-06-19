cf8cfae1ecb167ce2acf5fb0c10364978b499953
nohsunggyu <gkdlshtjdrb@naver.com>
2022-06-19 20:19:19 +0900

    코딩테스트 준비 기타 2번 문제 푸는 중

--- sunggyu/algorism/condigTest2/bruteforce/etc/Etc2.java
+++ sunggyu/algorism/condigTest2/bruteforce/etc/Etc2.java
@@ -11,6 +11,7 @@ public class Etc2{
     static int N;
     static int S;
     static int[] list;
+    static int length;
 
     static BufferedWriter bw;    public static void main(String[] args) throws Exception {
         bf = new BufferedReader(new InputStreamReader(System.in));
@@ -25,13 +26,75 @@ public class Etc2{
             list[i] = Integer.parseInt(split[i]);
             total += list[i];
         }  
+        length = getLength(total);
+        int temp = getLength2(total);
+        if(temp != 0){
+            if(length != 0){
+                length = Math.min(length, temp);
+            }else{
+                length = temp;
+            }
+        }
+        System.out.println(length);
 
 
         bw.flush();
         bw.close();
     }
 
-    public static void dfs(int total){
-        
+    public static int getLength(int total){
+        int length = N;
+        boolean flag = false;
+        for(int i = 0; i < N; i++){
+            total -= list[i];
+            if(total >= S){
+                length --;
+                flag = true;
+            }else{
+                total += list[i];
+                break;
+            }
+        }
+
+        for(int i = N-1; i >= 0; i--){
+            total -= list[i];
+            if(total >= S){
+                length --;
+                flag = true;
+            }else{
+                total += list[i];
+                break;
+            }
+        }
+        if(flag) return length;
+        return 0;
+    }
+
+    public static int getLength2(int total){
+        int length = N;
+        boolean flag = false;
+        for(int i = N-1; i >= 0; i--){
+            total -= list[i];
+            if(total >= S){
+                length --;
+                flag = true;
+            }else{
+                total += list[i];
+                break;
+            }
+        }
+
+        for(int i = 0; i < N; i++){
+            total -= list[i];
+            if(total >= S){
+                length --;
+                flag = true;
+            }else{
+                total += list[i];
+                break;
+            }
+        }
+        if(flag) return length;
+        return 0;
     }
 }
