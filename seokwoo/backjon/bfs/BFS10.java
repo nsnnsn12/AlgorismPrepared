e5bcd3aa45cbb4a36f312115a84193247f05591d
unknown <wjdtjrdn125@naver.com>
2022-06-19 22:11:31 +0900

    bfs 10번 해결

--- seokwoo/backjon/bfs/BFS10.java
+++ seokwoo/backjon/bfs/BFS10.java
@@ -6,165 +6,118 @@ import java.io.*;
 public class BFS10 {
 //https://www.acmicpc.net/problem/16197
 //두 동전
-	static boolean[][][] isVisit; // x,y,어떤동전이 방문했는지
-	static int[][][] countMatrix;
+	static boolean[][][][] isVisit; // 동전1x, 동전1y, 동전2x, 동전2y
 	static int[] dx = { 1, 0, -1, 0 };
 	static int[] dy = { 0, 1, 0, -1 };
 	static int n;
 	static int m;
-	
+	static char[][] matrix;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] def = br.readLine().split(" ");
+		n = Integer.parseInt(def[0]);
+		m = Integer.parseInt(def[1]);
+		matrix = new char[n][m];
+		isVisit = new boolean[n][m][n][m];
 
-		n = Integer.parseInt(def[0]); // 세로
-		m = Integer.parseInt(def[1]); // 가로
-		isVisit = new boolean[n][m][2];
-		countMatrix = new int[n][m][2];
-		char[][] matrix = new char[n][m];
-
-		int coinNo = 0;
-		Coin coin1 = null;
-		Coin coin2 = null;
+		boolean findOne = false;
+		int x1 = 0;
+		int x2 = 0;
+		int y1 = 0;
+		int y2 = 0;
 		for (int i = 0; i < n; i++) {
-			char[] def2 = br.readLine().toCharArray();
-			for (int j = 0; j < def2.length; j++) {
-				if (def2[j] == 'o') {
-					
-					if (coinNo == 0) {
-						coin1 = new Coin(coinNo, i, j);
+			matrix[i] = br.readLine().toCharArray();
+			for (int j = 0; j < m; j++) {
+				if (matrix[i][j] == 'o') {
+					if (!findOne) {
+						x1 = i;
+						y1 = j;
+						findOne = true;
 					} else {
-						coin2 = new Coin(coinNo, i, j);
+						x2 = i;
+						y2 = j;
+						break;
 					}
-					matrix[i][j] = def2[j];
-					coinNo++;
 				}
 			}
-			
-
 		}
-		Info info = new Info(coin1, coin2, matrix);
+		Info info = new Info(x1, y1, x2, y2, 0);
 		System.out.println(bfs(info));
+
 	}
 
-	private static int bfs(Info info) {
+	private static int bfs(Info firstInfo) {
 		Queue<Info> queue = new LinkedList<>();
-		queue.add(info);
-		int min = Integer.MAX_VALUE;
+		queue.add(firstInfo);
+		isVisit[firstInfo.x1][firstInfo.y1][firstInfo.x2][firstInfo.y2] = true;
+
 		while (!queue.isEmpty()) {
-			Info currentInfo = queue.poll();
+			Info info = queue.poll();
+			if (info.count >= 10) {	//count 10넘는 경우 -1 return
+				break;
+			}
 
 			for (int i = 0; i < dx.length; i++) {
-				Coin newCoin1 = new Coin(currentInfo.coin1.no,currentInfo.coin1.x + dx[i],currentInfo.coin1.y + dy[i]);
-				Coin newCoin2 = new Coin(currentInfo.coin2.no,currentInfo.coin2.x + dx[i],currentInfo.coin2.y + dy[i]);
-				int newCoin1No = currentInfo.coin1.no;
-				int newCoin2No = currentInfo.coin2.no;
-				int newCoin1X = currentInfo.coin1.x + dx[i];
-				int newCoin1Y = currentInfo.coin1.y + dy[i];
-				int newCoin2X = currentInfo.coin2.x + dx[i];
-				int newCoin2Y = currentInfo.coin2.y + dy[i];
-
-
-				if (isGo(newCoin1X, newCoin1Y, newCoin2X, newCoin2Y)) {
-					char[][] newMatrix = setNewMatrix(newCoin1X, newCoin1Y, newCoin2X, newCoin2Y, currentInfo.matrix);
-					if (isWall(newCoin1X, newCoin1Y, newCoin2X, newCoin2Y, newMatrix)) {
-						if ((!isVisit[newCoin1X][newCoin1Y][newCoin1No] && !isVisit[newCoin2X][newCoin2Y][newCoin2No])
-								|| countMatrix[currentInfo.coin1.x][currentInfo.coin1.y][currentInfo.coin1.no]
-										+ 1 < countMatrix[newCoin1X][newCoin1Y][newCoin1No]
-								|| countMatrix[currentInfo.coin2.x][currentInfo.coin2.y][currentInfo.coin2.no]
-										+ 1 < countMatrix[newCoin2X][newCoin2Y][newCoin2No]) {
-							
-							if(countMatrix[currentInfo.coin1.x][currentInfo.coin1.y][currentInfo.coin1.no]
-										+ 1 < countMatrix[newCoin1X][newCoin1Y][newCoin1No]) {
-								countMatrix[newCoin1X][newCoin1Y][newCoin1No] = countMatrix[currentInfo.coin1.x][currentInfo.coin1.y][currentInfo.coin1.no]
-										+ 1;
-							}
-							if(countMatrix[currentInfo.coin2.x][currentInfo.coin2.y][currentInfo.coin2.no]
-										+ 1 < countMatrix[newCoin2X][newCoin2Y][newCoin2No]) {
-								countMatrix[newCoin2X][newCoin2Y][newCoin2No] = countMatrix[currentInfo.coin2.x][currentInfo.coin2.y][currentInfo.coin2.no] + 1;
-							}
-							Info newInfo = new Info(newCoin1, newCoin2, newMatrix);
-							queue.add(newInfo);
-							isVisit[newCoin1X][newCoin1Y][newCoin1No] = true;
-							isVisit[newCoin2X][newCoin2Y][newCoin2No] = true;
-						}
+				int nx1 = info.x1 + dx[i];
+				int ny1 = info.y1 + dy[i];
+				int nx2 = info.x2 + dx[i];
+				int ny2 = info.y2 + dy[i];
+				int drop = 0; // 떨어진 동전 갯수
+				if (isGo(nx1, ny1)) {
+					if (matrix[nx1][ny1] == '#') {	//벽만나면 못감
+						nx1 = info.x1;
+						ny1 = info.y1;
 					}
-				}else {
-					if (0 > newCoin1X || newCoin1X >= n || 0 > newCoin1Y || newCoin1Y >= m) {
-						min = Math.min(min, countMatrix[currentInfo.coin1.x][currentInfo.coin1.y][currentInfo.coin1.no]+1);
+				}else {	//떨어진 동전 ++
+					drop++;
+				}
+
+				if (isGo(nx2, ny2)) {	//벽만나면 못감
+					if (matrix[nx2][ny2] == '#') {
+						nx2 = info.x2;
+						ny2 = info.y2;
 					}
-					if(0 > newCoin2X || newCoin2X >= n
-							|| 0 > newCoin2Y || newCoin2Y >= m) {
-						min = Math.min(min, countMatrix[currentInfo.coin2.x][currentInfo.coin2.y][currentInfo.coin2.no]+1);
-						
+				}else {	//떨어진 동전++
+					drop++;
+				}
+
+				if (drop == 1) {	//떨어진 동전 1개이면 이게 최소값(queue에 count를 add하므로 count 1인거 쭉 찾고, count 2 쭉 찾고.. 이과정을 반복하니 가장 먼저 나오는 return 값이 count 최소값)
+					return info.count + 1;
+				} else if (drop == 0) {//떨어진 동전 하나도 없으면
+					if (!isVisit[nx1][ny1][nx2][ny2]) {
+						isVisit[nx1][ny1][nx2][ny2] = true;
+						queue.add(new Info(nx1, ny1, nx2, ny2, info.count + 1));
 					}
 				}
 
 			}
-
 		}
-		return min;
+		return -1;
+
 	}
 
-	private static char[][] setNewMatrix(int newCoin1X, int newCoin1Y, int newCoin2X, int newCoin2Y, char[][] matrix) {
-		char[][] temp = new char[n][m];
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[i].length; j++) {
-				if(matrix[i][j] == 'o') {
-					temp[i][j] = '.';
-				}else {
-					temp[i][j] = matrix[i][j];
-				}
-				
-			}
-			temp[newCoin1X][newCoin1Y] = 'o';
-			temp[newCoin2X][newCoin2Y] = 'o';
-		}
-		return temp;
-	}
-
-	private static boolean isWall(int newCoin1X, int newCoin1Y, int newCoin2X, int newCoin2Y, char[][] matrix) {
-		if (matrix[newCoin1X][newCoin1Y] == '#' || matrix[newCoin2X][newCoin2Y] == '#') {
-			return false;
-		}
-		return true;
-	}
-
-	private static boolean isGo(int newCoin1X, int newCoin1Y, int newCoin2X, int newCoin2Y) {
-		if (0 <= newCoin1X && newCoin1X < n && 0 <= newCoin1Y && newCoin1Y < m && 0 <= newCoin2X && newCoin2X < n
-				&& 0 <= newCoin2Y && newCoin2Y < m) {
+	private static boolean isGo(int nx, int ny) {
+		if (0 <= nx && nx < n && 0 <= ny && ny < m) {
 			return true;
 		}
 		return false;
 	}
 
-	public static class Info {
-		Coin coin1;
-		Coin coin2;
-		char[][] matrix;
+	static private class Info {
+		int x1;
+		int y1;
+		int x2;
+		int y2;
+		int count;
 
-		public Info(Coin coin1, Coin coin2, char[][] matrix) {
-			this.coin1 = coin1;
-			this.coin2 = coin2;
-			this.matrix = new char[matrix.length][matrix[0].length];
+		public Info(int x1, int y1, int x2, int y2, int count) {
+			this.x1 = x1;
+			this.y1 = y1;
+			this.x2 = x2;
+			this.y2 = y2;
+			this.count = count;
 
-			for (int i = 0; i < matrix.length; i++) {
-				for (int j = 0; j < matrix[i].length; j++) {
-					this.matrix[i][j] = matrix[i][j];
-				}
-			}
-		}
-	}
-
-	public static class Coin {
-		int no;
-		int x;
-		int y;
-
-		public Coin(int no, int x, int y) {
-			this.no = no;
-			this.x = x;
-			this.y = y;
 		}
 	}
 
