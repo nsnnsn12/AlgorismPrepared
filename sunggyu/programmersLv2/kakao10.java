465506e2c054bb47851258f76a11e327e358f578
nohsunggyu <gkdlshtjdrb@naver.com>
2022-07-03 17:28:09 +0900

    카카오코테 2레벨 10번 문제 해결

--- sunggyu/programmersLv2/kakao10.java
+++ sunggyu/programmersLv2/kakao10.java
@@ -6,61 +6,77 @@ import java.util.*;
 class kakao10 {
     /*
         1 ~ n까지의 조합을 구한다.
-        n개의 조합으로 유일성이 만족하는 경우 n + 1부터는 해당 번호를 사용하지 않는다. 
+        유일성을 이미 만족하는 조합을 포함해서는 안 된다.
+        
+        8! = 4만
+        bfs로 풀지, dfs로 풀지
     */
     int length;
     boolean[] visited;
-    boolean[] temp;
-    int[] selected;
+    int[] selectList;
     String[][] relation;
     int answer;
+    List<int[]> candidateKeys = new ArrayList<>();
     public int solution(String[][] relation) {
         this.relation = relation;
         length = relation[0].length;
         visited = new boolean[length];
-        temp = new boolean[length];
-        for(int i  = 1; i <= length; i++){
-            selected = new int[i];
-            combo(0, 0, i);
-            for(int j  =0; j < temp.length; j++){
-                if(temp[j]) visited[j] = true;
-            }
+        for(int i = 1; i <= length; i++){
+            selectList = new int[i];
+            dfs(0, 0, i);
         }
         return answer;
     }
-    
-    public void combo(int startIndex, int depth, int count){
-        if(depth == count){
-            if(!isDuplicated()){
+    public void dfs(int startIndex, int depth, int selected){
+        if(depth == selected){
+            if(isUnique() && isMinimality()){
                 answer++;
-                for(int selectNo : selected){
-                    temp[selectNo] = true;
+                int[] key = new int[selected];
+                for(int i = 0; i < selected; i++){
+                    key[i] = selectList[i];
                 }
+                candidateKeys.add(key);
             }
             return;
         }
-        
         for(int i = startIndex; i < length; i++){
             if(!visited[i]){
                 visited[i] = true;
-                selected[depth] = i;
-                combo(i + 1, depth + 1, count);
+                selectList[depth] = i;
+                dfs(i+1, depth + 1, selected);
                 visited[i] = false;
             }
         }
     }
     
-    public boolean isDuplicated(){
-        HashSet<String> columns = new HashSet<>();
+    public boolean isUnique(){
+        HashSet<String> set = new HashSet<>();
         for(String[] list : relation){
             StringBuilder sb = new StringBuilder();
-            for(int selectNo : selected){
-                sb.append(list[selectNo]);
+            for(int i = 0; i < selectList.length; i++){
+                sb.append(list[selectList[i]]);
             }
-            columns.add(sb.toString());
+            set.add(sb.toString());
         }
-        if(columns.size() != relation.length) return true;
-        
+        if(set.size() == relation.length) return true;
         return false;
+    }
+    
+    public boolean isMinimality(){
+        if(selectList.length == 1) return true;
+        for(int[] key : candidateKeys){
+            if(key.length < selectList.length){
+                HashSet<Integer> set = new HashSet<>();
+                for(int select : selectList){
+                    set.add(select);
+                }
+                
+                for(int k : key){
+                    set.add(k);
+                }
+                if(selectList.length == set.size()) return false;
+            }
+        }
+        return true;
     }
 }
