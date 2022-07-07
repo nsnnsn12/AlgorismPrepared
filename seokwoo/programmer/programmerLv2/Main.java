bd9e9454f7817e68ee096edfb3599c78832359f3
unknown <wjdtjrdn125@naver.com>
2022-07-07 21:27:05 +0900

    kakao6 해결

--- seokwoo/programmer/programmerLv2/Main.java
+++ seokwoo/programmer/programmerLv2/Main.java
@@ -1,8 +1,5 @@
 package seokwoo.programmer.programmerLv2;
 
-/*  �Է�: ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
- *  ���: ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
- */
 public class Main {
 	public static void main(String[] args) {
 //		Greedy2 greedy2= new Greedy2();
@@ -11,10 +8,18 @@ public class Main {
 //		greedy2.solution(people, limit);
 		
 		
+		/*
 		Kakao5 k = new Kakao5();
 		String[] data = {"N~F=0","R~T>2"};
 		k.solution(2, data);
-
+		*/
+		Kakao6 k = new Kakao6();
+		String[] orders = {"XYZ", "XWY", "WXA"};
+		int[] course = {2,3,4};
+		String[] a= k.solution(orders, course);
+		for(int i = 0; i<a.length; i++) {
+			System.out.println(a[i]);
+		}
 	}
 
 }
