e601ea44677f425873ebd4c5e82f043432ace0d7
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-07-22 08:12:24 +0900

    카카오 코딩테스트 18번 문제 리팩토링

--- sunggyu/programmersLv2/kakao18.java
+++ sunggyu/programmersLv2/kakao18.java
@@ -3,86 +3,92 @@ package sunggyu.programmersLv2;
 import java.util.*;
 //https://school.programmers.co.kr/learn/courses/30/lessons/1835
 //단체사진 찍기
+/*
+    8개의 순열 중에 모든 조건을 만족하는 경우의 수를 리턴하라.
+    8! = 40,320
+    최대 조건의 갯수 = 100
+    4032000
+*/
 class kakao18 {
-    public ArrayList<char[]> perList = new ArrayList<char[]>();
-    public void per(boolean[] visited, char[] list, int depth, int n, char[] perlist){
-        if(depth == n){
-            char[] clist = new char[n];
-            for(int i = 0; i < n; i++){
-                clist[i] = perlist[i];
-            }
-            
-            perList.add(clist);
+    char[] friends = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+    static final int NUMBER_OF_FRIENDS = 8;
+    char[] selected = new char[NUMBER_OF_FRIENDS];
+    boolean[] visited = new boolean[NUMBER_OF_FRIENDS];
+    List<Condition> conditions = new ArrayList<>();
+    int answer;
+    public int solution(int n, String[] data) {
+        for(String condition : data){
+            conditions.add(new Condition(condition));
         }
-        for(int i = 0; i< n; i++){
+        findNumberOfRequirementsCases(0);
+        return answer;
+    }
+    
+    public void findNumberOfRequirementsCases(int depth){
+        if(depth == NUMBER_OF_FRIENDS){
+            for(Condition condition : conditions){
+                if(!condition.isRight(selected)) return;
+            }
+            answer++;
+            return;
+        }
+        
+        for(int i = 0; i < NUMBER_OF_FRIENDS; i++){
             if(!visited[i]){
-                perlist[depth] = list[i];
                 visited[i] = true;
-                per(visited, list, depth+1, n, perlist);
+                selected[depth] = friends[i];
+                findNumberOfRequirementsCases(depth+1);
                 visited[i] = false;
             }
         }
     }
-    public boolean check(String str, char[] clist){
-        boolean result = false;
-        char strCh = str.charAt(0);
-        int strIndex = 0;
-        char endCh = str.charAt(2);
-        int endIndex = 0;
-        //두 프렌즈사이의 간격 계산
-        for(int i = 0; i < 8; i++){
-            if(clist[i] == strCh){
-                strIndex = i;
-                break;
-            }
-        }
-        for(int i = 0; i < 8; i++){
-            if(clist[i] == endCh){
-                endIndex = i;
-                break;
-            }
-        }
-        int d = strIndex - endIndex;
-        if(d < 0) d *= -1;
-        d-=1;
-        int dist = str.charAt(4) - '0';
-        char calc = str.charAt(3);
-
-        switch(calc){
-            case '=':
-                if(d==dist) result = true;
-                break;
-            case '>':
-                if(d>dist) result = true;
-                break;
-            case '<':
-                if(d<dist) result = true;
-                break;
-        }
-        
-        return result;
-    }
-    public int solution(int n, String[] data) {
-        int answer = 0;
-        //8글자이므로 !8가능
-        char[] list = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
-        boolean[] visited = new boolean[8];
-        char[] perlist = new char[8];
-        //모든 순열의 조합 뽑기
-        per(visited, list, 0, 8, perlist);
-        
-        //조건을 만족하는 경우의 수 검색
-        for(char[] clist : perList){
-            boolean result = true;
-            //입력조건과 순열 비교
-            for(String str : data){
-                if(!check(str, clist)){
-                    result = false;
-                    break;
-                }
-            }
-            if(result) answer++;
-        }
-        return answer;
-    }
 }
+
+class Condition{
+    char me;
+    char friend;
+    int distance;
+    char condition;
+    public Condition(String condition){
+        char[] splitCondition = condition.toCharArray();
+        me = splitCondition[0];
+        friend = splitCondition[2];
+        distance = (int)(splitCondition[4]) - '0';
+        this.condition = splitCondition[3];
+    }
+    
+    public boolean isRight(char[] selected){
+        int myPosition = getPosition(selected, me);
+        int friendPosition = getPosition(selected, friend);
+        int nowDistance = Math.abs(myPosition - friendPosition);
+        if(condition == '=') return isEqualDistance(nowDistance);
+        
+        if(condition == '>') return isGraterThanDistance(nowDistance);
+        
+        if(condition == '<') return isLessThanDistance(nowDistance);
+        return false;
+    }
+    
+    public int getPosition(char[] selected, char c){
+        for(int i = 0; i < selected.length; i++){
+            if(c == selected[i]) return i;
+        }
+        
+        return 0;
+    }
+    
+    public boolean isEqualDistance(int nowDistance){
+        if(nowDistance-1 == distance) return true;
+        return false;
+    }
+    
+    public boolean isGraterThanDistance(int nowDistance){
+        if(nowDistance-1 > distance) return true;
+        return false;
+    }
+    
+    public boolean isLessThanDistance(int nowDistance){
+        if(nowDistance-1 < distance) return true;
+        return false;
+    }
+} 
