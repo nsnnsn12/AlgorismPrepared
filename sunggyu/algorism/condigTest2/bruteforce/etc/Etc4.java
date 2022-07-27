035b2d8fa8be7869f66f51e64b496dd5babb4308
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-07-27 14:34:34 +0900

    코딩테스트 준비 기타 4번 문제 해결

--- sunggyu/algorism/condigTest2/bruteforce/etc/Etc4.java
+++ sunggyu/algorism/condigTest2/bruteforce/etc/Etc4.java
@@ -15,6 +15,18 @@ import java.util.*;
 
     절반을 잘라 각각 모든 경우의 수를 구한다.
     그리고 이진 탐색을 이용하여 경우의 수를 탐색
+
+    
+    내가 생각하지 못 한 반례
+    1. 조합의 결과가 여러 개 있다는 것을 생각하지 못 함
+    3 0
+    0 0 0
+    7
+
+    2. result의 결과가 int의 범위를 넘을 수 있다는 것을 생각하지 못 함.
+    각 조합의 결과가 int의 범위를 넘을 수는 없지만 각 조합의 합은 int 범위를 넘을 수 있음.
+    40 0
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 */
 public class Etc4{
     static BufferedReader bf;
@@ -26,7 +38,8 @@ public class Etc4{
     static int[] list2;
     static List<Integer> comboList1 = new ArrayList<>();
     static List<Integer> comboList2 = new ArrayList<>();
-    static int result;
+    static List<int[]> test = new ArrayList<>();
+    static long result;
     public static void main(String[] args) throws Exception {
         bf = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -53,22 +66,48 @@ public class Etc4{
         //comboList2.stream().forEach(i -> System.out.print(i+" "));
         //System.out.println();
 
+        //comboList1.stream().forEach(i -> System.out.print(i+" "));
+        //System.out.println();
+
         Collections.sort(comboList2);
-        comboList2.stream().forEach(i -> System.out.print(i+" "));
-        System.out.println();
+        getList();
+        //test.stream().forEach(i -> System.out.print(i[0]+"|"+ i[1]+" "));
+        //System.out.println();
         for(int i : comboList1){
-            if(binarySearch(0, comboList2.size()-1, i, comboList2)) result++;
+            int index = binarySearch(0, test.size()-1, i, test);
+            if(index >= 0){
+                result += test.get(index)[1];
+            }
         }
         System.out.println(result);
         bw.flush();
         bw.close();
     }
 
-    public static boolean binarySearch(int startIndex, int endIndex, int selectedValue, List<Integer> list){
-        if(startIndex > endIndex) return false;
+    public static void getList(){
+        int selected = comboList2.get(0);
+        int count = 1;
+        for(int i = 1; i < comboList2.size(); i++){
+            if(selected == comboList2.get(i)){
+                count++;
+                continue;
+            }
+
+            int[] newInt = {selected, count};
+            test.add(newInt);
+            selected = comboList2.get(i);
+            count = 1;
+        }
+
+        int[] newInt = {selected, count};
+        test.add(newInt);
+    }
+
+    public static int binarySearch(int startIndex, int endIndex, int selectedValue, List<int[]> list){
+        if(startIndex > endIndex) return -1;
         int mid = (startIndex + endIndex) / 2;
-        int value = selectedValue + list.get(mid);
-        if(value == S) return true;
+        int value = selectedValue + list.get(mid)[0];
+        if(value == S) return mid;
 
         if(value > S){
             return binarySearch(startIndex, mid-1, selectedValue, list);
