c761c68a6e8720cda1ff8a284bcc59a630f97402
unknown <wjdtjrdn125@naver.com>
2022-07-31 11:08:09 +0900

    kakao 14번 해결(15x)

--- seokwoo/programmer/programmerLv2/Kakao14.java
+++ seokwoo/programmer/programmerLv2/Kakao14.java
@@ -1,38 +1,52 @@
 package seokwoo.programmer.programmerLv2;
-import java.util.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+//https://school.programmers.co.kr/learn/courses/30/lessons/17684
+//[3차]압축
 public class Kakao14 {
 	public int[] solution(String msg) {
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		HashMap<String, Integer> hs = new HashMap<String, Integer>();
-		int num;
-		for (num = 1; num <= ('Z' - 'A') + 1; num++) {
-			hs.put(Character.toString((char) ('A' + num - 1)), num);
-		}
+		ArrayList<Integer> array = new ArrayList<>();
+		HashMap<String, Integer> map = setMap();
 
 		int prev = 0;
+		int last = 27;
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < msg.length(); i++) {
 			sb.append(msg.charAt(i));
-			if (hs.containsKey(sb.toString())) {
-				prev = hs.get(sb.toString());
+
+			if (map.containsKey(sb.toString())) {
+				// System.out.println(sb.toString() + " " + map.get(sb.toString()));
+				prev = map.get(sb.toString());
 			} else {
-				hs.put(sb.toString(), num++);
-				arr.add(prev);
+				map.put(sb.toString(), last);
+				last++;
+				array.add(prev);
 				prev = 0;
 				sb.setLength(0);
 				i--;
 			}
-		}
 
+		}
 		if (prev != 0) {
-			arr.add(prev);
+			array.add(prev);
+		}
+		int[] answer = new int[array.size()];
+		for (int i = 0; i < array.size(); i++) {
+			answer[i] = array.get(i);
 		}
 
-		int[] answer = new int[arr.size()];
-		for (int i = 0; i < arr.size(); i++) {
-			answer[i] = arr.get(i);
-		}
 		return answer;
+	}
+
+	public HashMap<String, Integer> setMap() {
+		HashMap<String, Integer> map = new HashMap<>();
+		char alpha = 'A';
+		for (int i = 1; i < 27; i++) {
+			map.put(alpha + "", i);
+			alpha += 1;
+
+		}
+		return map;
 	}
 }
