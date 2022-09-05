4ce91c5d426642b0953904bda34f03517f80d716
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-09-05 22:00:01 +0900

    코딩테스트 준비 bfs 16번 문제 푸는 중

---
+++ sunggyu/algorism/condigTest2/bfs/Bfs16.java
@@ -0,0 +1,18 @@
+package sunggyu.algorism.condigTest2.bfs;
+import java.io.*;
+import java.util.*;
+//https://www.acmicpc.net/problem/9376
+//탈옥
+/*
+*/
+public class Bfs16{
+    static BufferedReader bf;
+    static BufferedWriter bw;
+    public static void main(String[] args) throws Exception {
+        bf = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.flush();
+        bw.close();
+    }
+}
