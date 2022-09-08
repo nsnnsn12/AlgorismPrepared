3a91e4d8e1cbad8af43598f9b2fa8b8e4b652ab4
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-09-08 18:47:07 +0900

    코딩테스트 준비 bfs 17번 문제 해결

--- sunggyu/algorism/condigTest2/bfs/Bfs16.java
+++ sunggyu/algorism/condigTest2/bfs/Bfs16.java
@@ -4,6 +4,9 @@ import java.util.*;
 //https://www.acmicpc.net/problem/9376
 //탈옥
 /*
+    2개의 좌표가 존재한다.
+    2개의 좌표가 최소한의 문을 열고 밖으로 갈 수 있는 문의 갯수는?
+    최소한은 어떤 조건을 만족하는가?
 */
 public class Bfs16{
     static BufferedReader bf;
