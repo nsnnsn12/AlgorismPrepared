78164f055e951c24a9b3dcd803766dc1c06076a9
unknown <wjdtjrdn125@naver.com>
2022-09-25 15:00:32 +0900

    kakao 레벨3 3번 해결

--- seokwoo/programmer/programmerLv3/Main.java
+++ seokwoo/programmer/programmerLv3/Main.java
@@ -3,9 +3,13 @@ package seokwoo.programmer.programmerLv3;
 public class Main {
 
 	public static void main(String[] args) {
+		/*
 		Kakao2 k = new Kakao2();
 		System.out.println(k.solution(new String[] {"frodo", "fradi", "crodo", "abc123", "frodoc"}, new String[] {"fr*d*", "abc1**"}));
-
+		*/
+		
+		Kakao3 k = new Kakao3();
+		System.out.println(k.solution(new int[] {3}, 3));
 	}
 
 }
