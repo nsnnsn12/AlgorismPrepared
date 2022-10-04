e12a75221e3a4ece7ef9a5f01022a1d9685bb153
unknown <wjdtjrdn125@naver.com>
2022-10-04 21:30:11 +0900

    kakao3 8번 푸는중

--- seokwoo/programmer/programmerLv3/Main.java
+++ seokwoo/programmer/programmerLv3/Main.java
@@ -18,7 +18,7 @@ public class Main {
 		k.solution(new String[] {"ZZZ", "YYY", "NNNN", "YYY", "BBB"});
 		*/
 		
-		
+		/*
 		Kakao5 k = new Kakao5();
 		int n = 6;
 		int s = 4;
@@ -26,6 +26,10 @@ public class Main {
 		int b = 2;
 		int[][] fares = {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}};
 		k.solution(n, s, a, b, fares);
+		*/
+		
+		Kakao8 k = new Kakao8();
+		k.solution(8, 2, new String[] {"D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"});
 	}
 
 }
