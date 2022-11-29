f908f34de2b128df518dd213a678679636c10793
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-11-29 21:14:10 +0900

    백준 단계별로 풀어보기 기본 수학 2 3번 문제 해결

--- sunggyu/backjun/algorism/math2/Math3.java
+++ sunggyu/backjun/algorism/math2/Math3.java
@@ -4,38 +4,26 @@ import java.io.*;
 
 public class Math3 {
     public static void main(String[] args) throws Exception {
+        //소인수분해란? 소인수(소수로 이루어진 인수) 들의 곱으로만 자연수를 나타낸 것을 뜻한다.
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int m = Integer.parseInt(bf.readLine());
         int n = Integer.parseInt(bf.readLine());
-        boolean[] isNotPrimeNumber = new boolean[10001];
+        boolean[] isNotPrimeNumber = new boolean[10000001];
         isNotPrimeNumber[1] = true;
-
-        for(int i = 2; i < 10001; i++){
+        for(int i = 2; i < 10000001; i++){
+            if(n == 1) break;
             if(isNotPrimeNumber[i]) continue;
+            while(n % i == 0){
+                bw.write(String.valueOf(i));
+                bw.newLine();
+                n /= i;
+            }
             int count = 2;
-            while(i * count <= 10000){
+            while(i * count <= 10000000){
                 isNotPrimeNumber[i*count] = true;
                 count++;
             }
         }
-        int result = 0;
-        int min = -1;
-        for(int i = m; i <= n; i++){
-            if(!isNotPrimeNumber[i]){
-                if(min == -1) min = i;
-                result += i;
-            }
-
-        }
-
-        if(min == -1) {
-            System.out.println(min);
-            return;
-        }
-
-        System.out.println(result);
-        System.out.println(min);
         bw.flush();
         bw.close();
     }
