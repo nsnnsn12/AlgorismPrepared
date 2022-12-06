76f684bfe4fc71c60b63a9fe95746d71a8dd044c
unknown <wjdtjrdn125@naver.com>
2022-12-06 22:26:33 +0900

    구현8번해결

--- seokwoo/backjon/implement2/Implement8.java
+++ seokwoo/backjon/implement2/Implement8.java
@@ -6,20 +6,7 @@ import java.io.*;
 // https://www.acmicpc.net/problem/17822
 // 원판 돌리기
 
-/*
- * 4 4 4
- * 1 1 2 3
- * 5 2 4 2
- * 3 1 3 5
- * 2 1 3 2
- * 2 0 1
- * 3 1 3
- * 2 0 2
- * 3 1 1
- */
 public class Implement8 {
-	private static int[] clock = { 3, 0, 1, 2 }; // 시계
-	private static int[] reverse = { 1, 2, 3, 0 }; // 반시계
 	private static int n; // 원판 개수
 	private static int m; // 원판에 적힌 번호 개수
 	private static int t; // 몇번 돌릴지
@@ -59,9 +46,9 @@ public class Implement8 {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				 result += array[i][j];
-	//			System.out.print(array[i][j] + " ");
+			//	System.out.print(array[i][j] + " ");
 			}
-		//	System.out.println();
+			//System.out.println();
 		}
 		 System.out.println(result);
 	}
@@ -71,28 +58,45 @@ public class Implement8 {
 		while (count < k) {
 			count++;
 
-			for (int i = x; i <= n; i = i * 2) {
+			for (int i = x; i <= n; i = i + x) {
 				int[] temp = setTemp(i - 1);
 
-				for (int z = 0; z < 4; z++) {
+				for (int z = 0; z < m; z++) {
 					if (d == 0) { // 시계
-						array[i - 1][z] = temp[clock[z]];
+						int index = z-1;
+						if(z == 0) {
+							index = m-1;
+						}
+						array[i - 1][z] = temp[index];
 
 					} else if (d == 1) { // 반시계
-						array[i - 1][z] = temp[reverse[z]];
+						int index = z+1;
+						if(z == m-1) {
+							index = 0;
+						}
+						array[i - 1][z] = temp[index];
 					}
 				}
 			}
 		}
+		/*
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		*/
 		boolean isNothing = false; // 인접한수가 없는경우
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if (array[i][j] != 0) {
 					isNothing = deleteNum(i, j, isNothing);
 				}
-
 			}
 		}
+		
 		if (!isNothing) {
 			double sum = 0;
 			double nocs = 0;
@@ -106,6 +110,7 @@ public class Implement8 {
 					}
 				}
 			}
+			
 			double avg = sum / nocs;
 			for(int i = 0; i<exist.size(); i++) {
 				int tx = exist.get(i)[0];
@@ -117,7 +122,7 @@ public class Implement8 {
 				}
 			}
 		}
-		
+		/*
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				System.out.print(array[i][j] + " ");
@@ -125,6 +130,8 @@ public class Implement8 {
 			System.out.println();
 		}
 		System.out.println();
+		*/
+		
 	}
 
 	private static boolean deleteNum(int x, int y, boolean isNothing) {
@@ -134,7 +141,7 @@ public class Implement8 {
 		queue.add(new int[] { x, y });
 		boolean[][] isVisit = new boolean[n][m];
 		int num = array[x][y];
-		//System.out.println(x + " " + y + " " + num) ;
+		
 		while (!queue.isEmpty()) {
 			int[] cur = queue.poll();
 			int curX = cur[0];
@@ -144,19 +151,21 @@ public class Implement8 {
 			for (int i = 0; i < dx.length; i++) {
 				int nx = curX + dx[i];
 				int ny = curY + dy[i];
-
-				if (isOut(nx, ny) || isVisit[nx][ny]) {
+				
+				if(ny == m) {
+					ny = 0;
+				}else if(ny == -1) {
+					ny = m-1;
+				}
+				if (isOut(nx,ny) || isVisit[nx][ny]) {
 					continue;
 				}
+				
 				if (num == array[nx][ny]) {
-					System.out.println(x + " " + y + " " + num) ;
 					queue.add(new int[] { nx, ny });
 					isVisit[nx][ny] = true;
 					array[x][y] = 0;
 					array[nx][ny] = 0;
-					if(nx == 2 && ny == 1) {
-						System.out.println(x + " "  + y + "dd");
-					}
 					temp = true;
 				}
 			}
@@ -165,7 +174,7 @@ public class Implement8 {
 	}
 
 	private static boolean isOut(int x, int y) {
-		if (0 <= x && x < n && 0 <= y && y < m) {
+		if(0<= x && x < n && 0<= y && y<m) {
 			return false;
 		}
 		return true;
@@ -178,5 +187,4 @@ public class Implement8 {
 		}
 		return temp;
 	}
-
 }
