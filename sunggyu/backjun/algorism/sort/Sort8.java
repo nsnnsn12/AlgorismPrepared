74de054e0d961c6c08dd462b94d071f4eaccd554
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-12-12 08:37:57 +0900

    백준 단계별로 풀어보기 정렬 8번 문제 해결

---
+++ sunggyu/backjun/algorism/sort/Sort8.java
@@ -0,0 +1,49 @@
+package sunggyu.backjun.algorism.sort;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Sort8 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(bf.readLine());
+        List<Point> points = new ArrayList<>();
+        for(int i = 0; i < N; i++){
+            String[] split = bf.readLine().split(" ");
+            int x = Integer.parseInt(split[0]);
+            int y = Integer.parseInt(split[1]);
+            points.add(new Point(x, y));
+        }
+
+        Collections.sort(points);
+
+        for(Point point : points){
+            bw.write(String.format("%d %d %n", point.x, point.y));
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static class Point implements Comparable<Point>{
+        int x,y;
+        public Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            if(o.y == y){
+                return x - o.x;
+            }
+
+            return y - o.y;
+        }
+
+    }
+    
+}
