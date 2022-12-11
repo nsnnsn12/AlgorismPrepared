c585a479fc2f15fc20cc42099d0bcc8124c4a16a
unknown <wjdtjrdn125@naver.com>
2022-12-11 16:58:36 +0900

    dp3 2번해결

--- seokwoo/backjon/dp3/DP2.java
+++ seokwoo/backjon/dp3/DP2.java
@@ -15,21 +15,26 @@ public class DP2 {
 			t--;
 			
 			int n = Integer.parseInt(br.readLine());
-			int[][] array = new int[2][n];
+			int[][] array = new int[2][n+1];
+			int[][] dp = new int[2][n+1];
 			
 			for(int i = 0; i<2; i++) {
 				String[] temp = br.readLine().split(" ");
 				
-				for(int z = 0; z<n; z++) {
-					array[i][z] = Integer.parseInt(temp[z]);
+				for(int z = 1; z <= n; z++) {
+					array[i][z] = Integer.parseInt(temp[z-1]);
 				}				
 			}
+			dp[0][1] = array[0][1];
+			dp[1][1] = array[1][1];
 			
-			
-			
-
+			for(int i = 2; i<=n; i++) {
+				dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2]) + array[0][i];
+				dp[1][i] = Math.max(dp[0][i-1], dp[0][i-2]) + array[1][i];
+			}
+			System.out.println(Math.max(dp[0][n], dp[1][n]));
 		}
-
+		
 	}
 
 }
